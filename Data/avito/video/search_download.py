c00d124133eb8f8229816d993f06f09684e7cd28
np2048 <np2048@gmail.com>
2019-12-16 16:21:44 +0300

    ..

--- Data/avito/video/search_download.py
+++ Data/avito/video/search_download.py
@@ -6,6 +6,9 @@ from bs4 import BeautifulSoup
 searchUrl = "https://www.avito.ru/krasnodar/tovary_dlya_kompyutera/komplektuyuschie/videokarty?cd=1&pmax=6000&pmin=1000"
 html = urlopen(searchUrl)
 bsSearch = BeautifulSoup(html.read(), "html.parser")
-bsPages = bsSearch.findAll("span", class_="pagination-item-1WyVp")
-for bsPage in bsPages :
-    print (bsPage.text)
+parsePageCount(bsSearch)
+
+def parsePageCount(bsSearch):
+    bsPages = bsSearch.findAll("span", class_="pagination-item-1WyVp")
+    for bsPage in bsPages :
+        print (bsPage.text)
