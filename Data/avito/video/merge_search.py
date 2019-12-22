aaa4590abc0c977deb4eaba8053c29b5b771e246
np2048 <np2048@gmail.com>
2019-12-22 22:46:30 +0300

    merge in progress

--- Data/avito/video/merge_search.py
+++ Data/avito/video/merge_search.py
@@ -4,6 +4,21 @@ import os
 import time
 from math import ceil
 import parse_search
+import re
+
+def strFilter(caption):
+    caption = caption.replace(
+            '-', ' ').replace(
+            '+', ' ').replace(
+            '/', ' ')
+    return (caption.replace('  ', ' '))
+
+def strSplit(caption):
+    return re.findall('(\w+|\d+)', caption)
+
+def findItem(captionWords, listNames):
+    for word in captionWords:
+
 
 # start timer and go
 timeStart = time.time()
@@ -14,14 +29,17 @@ listOrigin = parse_search.readCsv('gpulist.csv')
 
 # load search results 
 listSearch = parse_search.readCsv('search.csv')
-print(listSearch)
+#print(listSearch)
 
 # find gpu by name
 listRecognized = []
 listUnknown = []
 for item in listSearch:
     (caption, price, link) = item
-
+    caption = strFilter(caption)
+    captionWords = strSplit(caption)
+    #print(captionWords)
+    listResult = findItem(captionWords, listOrigin)
 
 # save merged list
 
